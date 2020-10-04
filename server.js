1b4d816d48b93397b43a825e470f3fd1682e5d8b
gracemang <gmang@uncc.edu>
2020-10-04 13:47:36 -0400

    Loading data from JSON file

--- server.js
+++ server.js
@@ -5,27 +5,12 @@ const port = 3000;
 
 app.use('/', express.static('public'));
 
-const budget = {
-    myBudget: [
-    {
-        title: 'Eat out', 
-        budget: 25
-    },
-    {
-        title: 'Rent', 
-        budget: 375
-    }, 
-    {
-        title: 'Grocery',
-        budget: 110
-    },
-  ]
-};
 app.get('/hello', (req, res) => {
     res.send('Hello World!');
 });
 
 app.get('/budget', (req, res) => {
+    const budget = require('./exercise.json');
     res.json(budget);
 });
 
